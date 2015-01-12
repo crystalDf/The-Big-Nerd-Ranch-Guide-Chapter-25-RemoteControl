efc93d93e5187e8926b211c3baeafd2fbd8a91de
crystalDf <chendong333@gmail.com>
2015-01-12 20:42:40 +0800

    Chapter 25\RemoteControl

---
+++ app/src/main/java/com/star/com/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,21 @@
+package com.star.com.remotecontrol;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Window;
+
+
+public class RemoteControlActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new RemoteControlFragment();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+    }
+
+}
